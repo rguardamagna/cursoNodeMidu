e9d860fcb320a8c99c63038efac1907bff3a4338
rguardamagna <r_guardamagna@hotmail.com>
2023-09-28 17:32:20 +0200

    postUpdate

--- clase2/2.routing.js
+++ clase2/2.routing.js
@@ -14,18 +14,30 @@ const processRequest = (req, res) => {
                     res.statusCode = 404
                     res.setHeader('Content-Type', 'text/html; charset=utf-8')
                     return res.end('<h1>404</h1>')
-
-
             }
+
         case 'POST':
             switch (url) {
                 case '/pokemon':
                     {
                         let body = ''
+                        req.on('data', chunk => {
+                            body += chunk.toString()
+                        })
+                        req.on('end', () => {
+                            const data = JSON.parse(body)
+                            // llamar a una base de datos para guardar la info
+                            res.writeHead(201, { 'Content-Type': 'application/son; charset=utf-8' })
+                            res.end(JSON.stringify(data))
+                        })
+                        //escuchar el evento data
+                        break
                     }
-                case '/otro': {
-                    let body = ''
-                }
+
+                default:
+                    res.statusCode = 404
+                    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+                    return res.end('<h1>404 Not Found</h1>')
             }
     }
 }

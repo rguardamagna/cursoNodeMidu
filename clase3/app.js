3e93a4ae9a18ca19e5e92a232d7017cebae480f2
rguardamagna <r_guardamagna@hotmail.com>
2023-10-02 17:16:45 +0200

    class3Update3

--- clase3/app.js
+++ clase3/app.js
@@ -2,25 +2,13 @@ const express = require('express')
 const crypto = require('node:crypto')
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
 app.disable('x-powered-by')
-
-app.get('/', (req, res) => {
-  res.json({ message: 'hola mundo' })
-})
-
-app.get('/movies', (req, res) => {
-  const { genre } = req.query
-  if (genre) {
-    const filteredMovies = movies.filter(
-      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-    )
-    return res.json(filteredMovies)
-  }
-  res.status(201).json(movies)
-})
 
 app.get('/movies/:id', (req, res) => { // path-to-regex
   const { id } = req.params
@@ -49,6 +37,19 @@ app.post('/movies', (req, res) => {
   movies.push(newMovie)
 
   res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: '404 Not Found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
 })
 
 app.patch('/movies/:id', (req, res) => {

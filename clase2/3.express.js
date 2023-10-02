281b20fec2ac342640ccf54ab508503cb0f2b623
rguardamagna <r_guardamagna@hotmail.com>
2023-10-02 15:20:27 +0200

    class3Update2

--- clase2/3.express.js
+++ clase2/3.express.js
@@ -14,11 +14,11 @@ app.disable('x-powered-by')
 // })
 
 // esto hace lo mismo (evalua POST y Cont Type y devuelve el chunk - no el datetime que es lógica personalizada)
-//app.use(express.json()) 
-app.use((req, res, next)=>{ // se puede usar en un metodo en concreto o para urls especificas
+// app.use(express.json())
+app.use((req, res, next) => { // se puede usar en un metodo en concreto o para urls especificas
     if (req.method !== 'POST') return next()
     if (req.headers['content-type'] !== 'application/json') return next()
-    
+
     // solo llegan POST y application/json
     let body = ''
     req.on('data', chunk => {
@@ -28,26 +28,24 @@ app.use((req, res, next)=>{ // se puede usar en un metodo en concreto o para url
         const data = JSON.parse(body)
         // llamar a una base de datos para guardar la info
         data.datetime = Date.now()
-        req.body=data
+        req.body = data
         next()
     })
 })
 
-app.get('/pokemon/ditto', (req,res) => {
-    //res.json({ message: 'hola mundo'})
+app.get('/pokemon/ditto', (req, res) => {
+    // res.json({ message: 'hola mundo'})
     res.json(ditto) // no es necesario declarar el Contet-Type
-    
 })
 
-app.post('/pokemon', (req,res) => {
+app.post('/pokemon', (req, res) => {
     res.status(201).json(req.body)
 })
 
-app.use((req,res)=>{
+app.use((req, res) => {
     res.status(404).send('<h1>404 Not Found</h1>')
 })
 
 app.listen(PORT, () => {
     console.log(`server listening on port http://localhost:${PORT}`)
 })
-
--- clase3/app.js
+++ clase3/app.js
@@ -1,36 +1,81 @@
 const express = require('express')
+const crypto = require('node:crypto')
 const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
+app.use(express.json())
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => { 
-    res.json({message:'hola mundo'})
+app.get('/', (req, res) => {
+  res.json({ message: 'hola mundo' })
 })
 
-app.get('/movies', (req, res) => { 
-    const { genre } = req.query
-    if (genre) { 
-        const filteredMovies = movies.filter(
-            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-        )
-        return res.json(filteredMovies)
-    }
-    res.status(201).json(movies)
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.status(201).json(movies)
 })
 
-app.get('/movies/:id', (req, res) => { //path-to-regex
-    const { id } = req.params
-    const movie = movies.find(movie => movie.id === id) 
-    if (movie) return res.json(movie)
-    
-    res.status(404).json({message: '404 Not Found'})
-    
-    res.status(201).json(movies[{id}])
+app.get('/movies/:id', (req, res) => { // path-to-regex
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: '404 Not Found' })
+
+  res.status(201).json(movies[{ id }])
 })
 
-const PORT = process.env.PORT ?? 1234
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
 
+  if (result.error) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // Luego a base de datos
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 1235
 app.listen(PORT, () => {
-    console.log(`listening in port http://localhost:${PORT}`)
+  console.log(`listening in port http://localhost:${PORT}`)
 })

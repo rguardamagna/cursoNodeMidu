1c321246a661c859e5a98ebc846d8d439f4409dc
rguardamagna <r_guardamagna@hotmail.com>
2023-10-17 16:58:30 +0200

    updateController1.0

--- clase4/models/movie.js
+++ clase4/models/movie.js
@@ -1,6 +1,6 @@
-import { readJSON } from '../utils.js'
 import { randomUUID } from 'node:crypto'
-const movies = readJSON('../movies.json')
+import { readJSON } from '../utils.js'
+const movies = readJSON('./movies.json')
 
 export class MovieModel {
   static async getAll({ genre }) {
--- clase4/routes/movies.js
+++ clase4/routes/movies.js
@@ -1,56 +1,11 @@
 import { Router } from 'express'
-import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
-import { MovieModel } from '../models/movie.js'
+
+import { MovieController } from '../controllers/movie.js'
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', async (req, res) => {
-  const { genre } = req.query
-  const movies = await MovieModel.getAll({ genre })
-  res.json(movies)
-})
-
-moviesRouter.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const movie = await MovieModel.getById({ id })
-  if (movie) return res.json(movie)
-  res.status(404).json({ message: '404 Not Found' })
-})
-
-moviesRouter.post('/', async (req, res) => {
-  const result = validateMovie(req.body)
-  if (result.error) {
-    // 422 Unprocessable Entity
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-  // Luego a base de datos
-
-  const newMovie = await MovieModel.create({ input: result.data })
-
-  res.status(201).json(newMovie)
-})
-
-moviesRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-
-  const result = await MovieModel.delete({ id })
-
-  if (result === false) {
-    return res.status(404).json({ message: '404 Not Found' })
-  }
-
-  return res.json({ message: 'Movie deleted' })
-})
-moviesRouter.patch('/movies/:id', async (req, res) => {
-  const result = validatePartialMovie(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-
-  const updateMovie = await MovieModel.update({ id, input: result.data })
-
-  return res.json(updateMovie)
-})
+moviesRouter.get('/', MovieController.getAll)
+moviesRouter.get('/:id', MovieController.getById)
+moviesRouter.post('/', MovieController.create)
+moviesRouter.delete('/:id', MovieController.delete)
+moviesRouter.patch('/movies/:id', MovieController.update)
